cc5461cc278afb94a714ca36377e7093faa88cd2
Greta Žebrauskaitė <greta.zebrauskaite@gmail.com>
2017-11-14 19:32:18 +0200

    no message

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'footer',
     styleUrls: ['footer.component.css'],
-    template: `<div class="custom-border">
-        <div class="text-center mb-3 mt-3">Knygos</div>
-    </div>`,
+    templateUrl: './footer.component.html',
 })
 export class FooterComponent { }
